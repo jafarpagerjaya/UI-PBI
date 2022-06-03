503c56cc86c8fed33bcc83d2162377ddcc17a59e
unknown <jafarpager@gmail.com>
2022-06-03 20:11:31 +0700

    menambah bug pada datepicker decade dan century tinggal bug pada kosongkan

--- admin/pages/js/tambah-donasi.js
+++ admin/pages/js/tambah-donasi.js
@@ -23,7 +23,9 @@ waktu_bayar.querySelector('.input-group-append').addEventListener('click', funct
 // let d = new Date(); d.setDate(d.getDate());
 
 let d = new Date('08/11/2021');
-$('.datepicker').datepicker('setStartDate', d);
+
+// extended function for datepicker
+const dateRanges = (date = new Date(), rule = 10, sum = 0) => Math.floor(date.getFullYear() / rule) * rule + sum;
 
 $('.datepicker').datepicker({
     todayBtn: "linked",
@@ -32,6 +34,47 @@ $('.datepicker').datepicker({
     format: 'd MM yyyy'
 }).change(function(e) {
     // submitControl(e.target);
-}).on('changeYear', function(e) {
-    console.log(e)
-});
+}).on('show', function(e,x) {
+    let allowedPicker = [],
+        untilPicker = undefined,
+        year = undefined,
+        timeEpoc;
+    if (e.viewMode == 3) {
+        // Start From This(d) Decade
+        allowedPicker = [dateRanges(d)];
+        // Until This(default dateRanges) Decade
+        untilPicker = dateRanges();
+        year = 10;
+        timeEpoc = 'decade';
+    } else if (e.viewMode == 4) {
+        // daterange second params set to 100 a century
+        // Start From This(d) Century
+        allowedPicker = [dateRanges(d, 100)];
+        // Until This(now) Century
+        untilPicker = dateRanges(new Date(), 100);
+        year = 100;
+        timeEpoc = 'century';
+    }
+    if (allowedPicker.length > 0) {
+        if (allowedPicker.find(element => element == untilPicker) == undefined) {
+            allowedPicker.push(untilPicker)
+        }
+        if (allowedPicker.length > 1 && allowedPicker[1] - allowedPicker[0] > year) {
+            let loop = 1;
+            do {
+                if (allowedPicker.find(element => element == (loop*year)) == undefined) {
+                    allowedPicker.push(allowedPicker[loop-1]+year);
+                }
+                loop++;
+            } while ((allowedPicker[1] - allowedPicker[0]) / year > loop);
+            allowedPicker.sort();
+        }
+        allowedPicker.forEach(pickElement => {
+            $('.datepicker.datepicker-dropdown table tbody td>span.disabled:contains('+ pickElement +')').removeClass('disabled');
+        });
+    }
+    $('.datepicker.datepicker-dropdown').on('click', 'tfoot .clear', function(e) {
+        e.stopPropagation();
+        console.log('anjir')
+    })
+}).datepicker('setStartDate', d);
